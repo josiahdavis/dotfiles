865661ec2596d22566cfa06231cabbb6b3a39396
Josiah Davis <josiah.j.davis@gmail.com>
2020-04-21 04:19:24 +0000

    simple script for linking to dot files

---
+++ link_dot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.git_template $HOME/.git_template

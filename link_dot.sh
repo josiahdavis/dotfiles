a59321412116a5fbd96c3d3d8615aa6a8b552ec1
Josiah Davis <josiah.j.davis@gmail.com>
2020-09-16 11:42:31 +0800

    initial commit for zshrc

--- link_dot.sh
+++ link_dot.sh
@@ -5,3 +5,4 @@ ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.git_template $HOME/.git_template
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc

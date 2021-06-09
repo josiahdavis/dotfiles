764d77f25c861e7380976f6ee79ededb6a46cf25
Josiah Davis <josiah.j.davis@gmail.com>
2021-06-09 11:02:27 +0800

    Remove linking for git config, since handled elsewhere

--- link_dot.sh
+++ link_dot.sh
@@ -3,6 +3,6 @@
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/.git_template $HOME/.git_template
+# ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+# ln -s $HOME/dotfiles/.git_template $HOME/.git_template
 # ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
